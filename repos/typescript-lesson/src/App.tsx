a8cf1a614212101a884e262d0e55229a7c4fe648
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-10-28 11:08:53 -0300

    feat: add minimal error example

--- repos/typescript-lesson/src/App.tsx
+++ repos/typescript-lesson/src/App.tsx
@@ -1,6 +1,15 @@
 import "./App.css";
 
+type User = {
+  name: string;
+  surname: string;
+};
+
 function App() {
+  const users: User[] = [
+    { name: "Jose", surname: "Verdura" },
+    { name: 99, surname: "Flama" },
+  ];
   return (
     <main>
       <h1>Typescript lesson</h1>
